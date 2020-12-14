90771fa99498aa9177dc13eafc0eb1da9b46c840
fantoff2010@yandex.ru <fantoff2010@yandex.ru>
2020-12-14 21:30:47 +0300

    Fixed sorting.

--- main.py
+++ main.py
@@ -27,11 +27,10 @@ def find_intersection(edges_list, point_index):
             visited_list.append(True)
         else:
             visited_list.append(False)
-    # import pdb; pdb.set_trace()
+
     while queue:
-        point = int(queue.popleft())
+        point = queue.popleft()
         for index in edges_list[point]:
-            # import pdb; pdb.set_trace()
             if not visited_list[index]:
                 intersections[index] = intersections[point] + 1
                 visited_list[index] = True
@@ -44,48 +43,52 @@ def graph(triangles):
     indexes = make_indexes_for_edges(triangles)
     for (index, triangle) in enumerate(triangles):
         for first_edge_point in triangle:
-            first_edge_point = np_array_to_tuple(first_edge_point)
+            first_edge_point = indexes[np_array_to_tuple(first_edge_point)]
             for second_edge_point in triangle:
-                second_edge_point = np_array_to_tuple(second_edge_point)
-                if first_edge_point[0] < second_edge_point[0]:  #Возможно ошибка в сортировке
-                    edges_list.append((indexes[first_edge_point],
-                                       indexes[second_edge_point], index))
-                elif first_edge_point[0] == second_edge_point[0] \
-                        and first_edge_point[1] > second_edge_point[1]:
+                second_edge_point = indexes[np_array_to_tuple(second_edge_point)]
+                if first_edge_point < second_edge_point:  #Возможно ошибка в сортировке
+                    edges_list.append((first_edge_point, second_edge_point, index))
+                # elif first_edge_point[0] == second_edge_point[0] \
+                #         and first_edge_point[1] > second_edge_point[1]:
 
-                    edges_list.append((indexes[first_edge_point],
-                                       indexes[second_edge_point], index))
-
-    edges_list = sorting_edges(edges_list)
-
+                #     edges_list.append((indexes[first_edge_point],
+                #                        indexes[second_edge_point], index))
+    sorted_edges_list = sorting_edges(edges_list)
+    # assert edges_list_v1 == edges_list_v2
     new_edges = [[] for a in range(triangles.shape[0])]
-    for i in range(len(edges_list) - 1):
-        if edges_list[i][:-1] == edges_list[i + 1][:-1]:
-            new_edges[edges_list[i][-1]].append(edges_list[i + 1][-1])
-            new_edges[edges_list[i + 1][-1]].append(edges_list[i][-1])
+    for i in range(len(sorted_edges_list) - 1):
+        if sorted_edges_list[i][:-1] == sorted_edges_list[i + 1][:-1]:
+            new_edges[sorted_edges_list[i][-1]].append(sorted_edges_list[i + 1][-1])
+            new_edges[sorted_edges_list[i + 1][-1]].append(sorted_edges_list[i][-1])
     return new_edges
 
 
 def sorting_edges(edges_list):
-    number_of_elements = len(edges_list)
-    middle_index = number_of_elements // 2
-    half_of_edges = [0] * middle_index
+    queue = deque()
+    queue.append([edges_list, 1])
+    while queue:
+        list_for_sorting, sorting_id = queue.pop()
+        number_of_elements = len(list_for_sorting)
+        middle_index = number_of_elements // 2
+        half_of_edges = [0] * middle_index
 
-    sorted_edges = [0 for a in range(number_of_elements)]
-
-    for sorting_id in reversed(range(0, 2)):
+        sorted_edges = [0 for a in range(number_of_elements)]
         for i in range(number_of_elements):
-            key = edges_list[i][sorting_id]
+            key = list_for_sorting[i][sorting_id]
             half_of_edges[key % middle_index] += 1
 
         for i in range(1, middle_index):
             half_of_edges[i] += half_of_edges[i - 1]
 
         for i in range(number_of_elements):
-            key = edges_list[i][sorting_id]
+            key = list_for_sorting[i][sorting_id]
             sorted_edges[number_of_elements - half_of_edges[key
-                         % middle_index]] = edges_list[i]
+                         % middle_index]] = list_for_sorting[i]
             half_of_edges[key % middle_index] -= 1
+        if sorting_id == 0:
+            return sorted_edges
+        queue.append([sorted_edges, 0])
+
     return sorted_edges
 
 
